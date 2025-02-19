e60b2f51fecc5fa4820fab896b1c436f83b94fe5
Matt Plummer <matt.plummer@gmail.com>
2025-02-19 23:28:36 +0000

    removing some straggler php files

--- convert_to_static.php
+++
@@ -1,9 +0,0 @@
-
-<!DOCTYPE html>
-
-<?php ob_start(); // start output buffering ?>
-
-<?php include ('index.php')?>
-
-<?php file_put_contents("index.html", ob_get_contents()); // write the contents of the buffer to file
-ob_end_clean(); // stop output buffering ?>
